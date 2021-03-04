a223deae37093ab4921c5c590010ed58fbd43245
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2021-03-04 17:42:02 -0500

    define canadaCA CSS styles in separate module

---
+++ utils/canada_ca_css_themes.js
@@ -0,0 +1,12 @@
+import { css } from "@emotion/core";
+
+export const anchor = css`
+  color: #284162;
+  text-decoration: underline;
+  background-color: transparent;
+  :hover,
+  :focus {
+    color: #0535d2;
+    text-decoration: underline;
+  }
+`;
